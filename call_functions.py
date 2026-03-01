f383817bc7d48ee27aef7da1cf432f99018a76fc
jeringeorge98 <jerringeorge439@gmail.com>
2026-03-01 14:35:44 +0200

    py linting

--- call_functions.py
+++ call_functions.py
@@ -1,9 +1,7 @@
 from google.genai import types
-
 from fuctions.get_files_content import schema_get_files_content
 from fuctions.run_python_file import schema_run_python_file
 from fuctions.write_file_content import schema_write_file_content
-from fuctions.get_files_info import schema_get_files_info
 
 available_functions = types.Tool(
     function_declarations=[schema_get_files_content,schema_run_python_file,schema_write_file_content],
@@ -17,4 +15,4 @@ def call_function(function_call : types.FunctionCall, verbose=False):
         print(f"Calling function: {function_call.name}({function_call.args})")
     else :
         print(f" - Calling function: {function_call.name}")
-             
+
